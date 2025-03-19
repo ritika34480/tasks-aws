d17e0b872746386f66fffb859893b6535549310d
ritika_pal <ritika_pal@epam.com>
2025-03-19 16:37:41 +0530

    commit task04

---
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -0,0 +1,20 @@
+package com.task04;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+import java.util.HashMap;
+import java.util.Map;
+@LambdaHandler(   lambdaName = "sns_handler",    roleName = "sns_handler-role",    isPublishVersion = true,    aliasName = "${lambdas_alias_name}",    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+@SnsEventSource(       targetTopic = "lambda_topic")
+public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
+ public Map<String, Object> handleRequest(Object request, Context context) {
+      context.getLogger().log("Request: " + request.toString());
+
+          Map<String, Object> resultMap = new HashMap<String, Object>();
+            resultMap.put("statusCode", 200);
+             resultMap.put("message", "Processed SNS message successfully");
+              return resultMap;
+              }
+              }
---
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -0,0 +1,19 @@
+package com.task04;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.RetentionSetting;
+import java.util.HashMap;
+import java.util.Map;
+@LambdaHandler(    lambdaName = "sqs_handler",    roleName = "sqs_handler-role",    isPublishVersion = true,    aliasName = "${lambdas_alias_name}",    logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+@SqsTriggerEventSource(       targetQueue = "async_queue",       batchSize = 10)
+public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
+  public Map<String, Object> handleRequest(Object request, Context context) {
+    context.getLogger().log("Request: " + request.toString());
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+             resultMap.put("statusCode", 200);
+                resultMap.put("message", "Processed SQS message successfully");
+                    return resultMap;
+                      }
+                      }
