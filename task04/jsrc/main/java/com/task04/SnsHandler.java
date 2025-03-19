89167cadafa1def3c601360490b865b65d008beb
ritika_pal <ritika_pal@epam.com>
2025-03-19 11:34:34 +0530

    Initial commit

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -1,27 +1,27 @@
 package com.task04;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
+
 import java.util.HashMap;
 import java.util.Map;
+
 @LambdaHandler(
-		lambdaName = "sns_handler",
-		roleName = "sns_handler-role",
-		isPublishVersion = true,
-		aliasName = "learn",
-		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
-)
-@SnsEventSource(
-		targetTopic = "lambda_topic"
+    lambdaName = "SnsHandler",
+	roleName = "SnsHandler-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
+
 	public Map<String, Object> handleRequest(Object request, Context context) {
-		context.getLogger().log("Received event: " + request.toString() + "\n");
-		Map<String, Object> resultMap = new HashMap<>();
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Processed SNS Messages Successfully");
+		resultMap.put("body", "Hello from Lambda");
 		return resultMap;
 	}
 }
